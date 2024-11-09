0f107ee5179e3147586c82fc208efbe08d557c82
Chinmaya0011 <imchinu17@gmail.com>
2024-11-09 10:13:54 +0530

    update

--- client/my-agency/vite.config.js
+++ client/my-agency/vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure the app works properly when deployed in the root path
 })
