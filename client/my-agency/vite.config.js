f84d4a3212a17048e09b3561dfb8e66e6c96b1f4
Chinmaya0011 <imchinu17@gmail.com>
2024-11-09 11:01:49 +0530

    update

--- client/my-agency/vite.config.js
+++ client/my-agency/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Define base dynamically based on the environment
 export default defineConfig({
   plugins: [react()],
+  base :'/', // Set BASE_URL via environment variable or default to '/'
 })
