ccfffb20c0496aff51efbf4b1babc08274fa1c72
Chinmaya0011 <imchinu17@gmail.com>
2024-11-13 01:31:33 +0530

    update

--- client/my-agency/src/App.jsx
+++ client/my-agency/src/App.jsx
@@ -9,18 +9,7 @@ import { AuthProvider } from './context/AuthContext';
 import { FormProvider } from './context/FormContext';
 import { ContactFormProvider } from './context/ContactFormContext';
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
 
-  // Simulating page loading logic without setTimeout
-  useEffect(() => {
-    // You can replace this with actual logic such as fetching data, or waiting for the app to be ready
-    setIsLoading(false); // Immediately set loading to false after initial load
-  }, []);
-
-  // Render the loading component while loading
-  if (isLoading) {
-    return <Loading />;
-  }
 
   // Render the app routes after loading
   return (
