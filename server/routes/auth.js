51e9c7311f65ffb6514278f811d7342e192e83b8
Chinmaya0011 <imchinu17@gmail.com>
2024-11-15 17:21:57 +0530

    Fix casing for user model

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 const dotenv = require('dotenv');
 const verifyToken = require('../middleware/verifyToken'); // Import the verifyToken middleware
